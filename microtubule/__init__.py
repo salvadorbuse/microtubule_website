f6169e5cab2d8ff8e7fabe990867fdb46a6b019f
emlarsson <66221097+emlarsson@users.noreply.github.com>
2020-12-08 16:28:03 +0100

    Create __init__.py

---
+++ microtubule/__init__.py
@@ -0,0 +1,10 @@
+"""Docstring goes here!"""
+
+from .module1 import *
+from .module2 import *
+from .module3 import *
+
+__author__ = 'group 17'
+__email__ = 'sbuse@caltech.edu'
+__version__ = '0.0.1'
+__license__ = 'MIT'
